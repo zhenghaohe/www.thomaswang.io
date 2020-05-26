c2b777046fc846ab46cf865762450679269194c8
Thomas Wang <iothomaswang@gmail.com>
2020-05-26 01:40:04 -0700

    add try catchg

--- src/components/ViewCounter.js
+++ src/components/ViewCounter.js
@@ -7,33 +7,41 @@ const ViewCounter = ({ id }) => {
   const [views, setViews] = useState("")
 
   useEffect(() => {
-    const onViews = newViews => setViews(newViews.val())
-    let db
+    try {
+      const onViews = newViews => setViews(newViews.val())
+      let db
 
-    const fetchData = async () => {
-      db = await loadDb()
-      db.child(id).on("value", onViews)
-    }
-
-    fetchData()
-
-    return () => {
-      if (db) {
-        db.child(id).off("value", onViews)
+      const fetchData = async () => {
+        db = await loadDb()
+        db.child(id).on("value", onViews)
       }
+
+      fetchData()
+
+      return () => {
+        if (db) {
+          db.child(id).off("value", onViews)
+        }
+      }
+    } catch (error) {
+      console.log(error)
     }
   }, [id])
 
   useEffect(() => {
-    const registerView = () =>
-      fetch(
-        `https://us-central1-thomas-wang.cloudfunctions.net/views?id=${id.replace(
-          /\\|\//g,
-          ""
-        )}`
-      )
+    try {
+      const registerView = () =>
+        fetch(
+          `https://us-central1-thomas-wang.cloudfunctions.net/views?id=${id.replace(
+            /\\|\//g,
+            ""
+          )}`
+        )
 
-    registerView()
+      registerView()
+    } catch (error) {
+      console.log(error)
+    }
   }, [id])
 
   return `${views ? format(views) : "–––"} views`
