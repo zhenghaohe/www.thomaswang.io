7a0497072ec7f4a19cad589f47b2006795d61fa0
Thomas Wang <iothomaswang@gmail.com>
2020-06-04 06:11:34 -0700

    add back cover

--- src/pages/about.js
+++ src/pages/about.js
@@ -66,9 +66,10 @@ const Landscape = styled.div`
     content: "";
     position: absolute;
     width: 100%;
+    height: 100%;
     top: 0;
     left: 0;
-    background: rgba(0, 0, 0, 0.5);
+    background: rgba(0, 0, 0, 0.3);
     opacity: 0;
     transition: all 0.5s;
   }
